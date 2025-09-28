86c57c4984a4e3c163fb06f01c34c75369c1afa7
BARAAH MEKYASH <BaraahMekyash@gmail.com>
2025-09-28 16:45:47 +0300

    Refactor input prompts for clarity and consistency

--- interset.py
+++ interset.py
@@ -2,12 +2,23 @@ p = 0
 r = 0
 t = 0
 
-#r = int(input("Enter the interset rate per year: "))#interset rate
-#t = int(input("Enter the time period: ")) #time period elapsed
-
 while p <= 0:
-    p = float(input("Enter the principle value: "))#inital balance
+    p = float(input("Enter the principle value: "))
     if p <=0 :
         print("can't be less that or equal to 0")
 
-print(f"The total amount you owe: {p}")
+
+while r <= 0:
+    r = float(input("Enter the principle value: "))
+    if r <=0 :
+        print("can't be less that or equal to 0")
+
+
+
+while t <= 0:
+    t = float(input("Enter the time in years: "))
+    if t <=0 :
+        print("can't be less that or equal to 0")
+
+totalBalance = p * pow((1+ r /100), t)
+print(f"Balance after {t} years is : ${totalBalance:.2f}")
