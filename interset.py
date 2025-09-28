4ef9c71a3c5fe15b6834a00caa70231d01d652d8
BARAAH MEKYASH <BaraahMekyash@gmail.com>
2025-09-28 16:47:56 +0300

    Refactor input validation loops for principle, interest rate, and time

--- interset.py
+++ interset.py
@@ -2,23 +2,25 @@ p = 0
 r = 0
 t = 0
 
-while p <= 0:
+while True:
     p = float(input("Enter the principle value: "))
     if p <=0 :
         print("can't be less that or equal to 0")
-
-
-while r <= 0:
+    else:
+        break
+while True:
     r = float(input("Enter the interset rate: "))
     if r <=0 :
         print("can't be less that or equal to 0")
+    else:
+        break
 
-
-
-while t <= 0:
+while True:
     t = float(input("Enter the time in years: "))
     if t <=0 :
         print("can't be less that or equal to 0")
+    else:
+        break
 
 totalBalance = p * pow((1+ r /100), t)
 print(f"Balance after {t} years is : ${totalBalance:.2f}")
